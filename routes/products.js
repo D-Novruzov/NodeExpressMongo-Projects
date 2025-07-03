34186db6a1db2dc11d57e32034bf1c4d6fafcab3
David Novruzov <novruzovdavcho@gmail.com>
2025-07-03 19:44:53 +0400

    project added

---
+++ routes/products.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const productsController = require("../controllers/products");
+router.route("/").get(productsController.getAllProducts);
+router.route("/static").get(productsController.getAllProductsStatic);
+module.exports = router;
