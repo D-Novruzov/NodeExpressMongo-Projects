57e5d97d72e558aa468ada1ac540229d197c2f8a
David Novruzov <novruzovdavcho@gmail.com>
2025-07-03 18:59:00 +0400

    Delete routes directory

--- routes/products.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productsController = require("../controllers/products");
-router.route("/").get(productsController.getAllProducts);
-router.route("/static").get(productsController.getAllProductsStatic);
-module.exports = router;
